5d9f5071dc946370250e8d06f5b6fb1514896ada
Diego Lopes da Silva <diego.silva.09lopes@gmail.com>
2026-05-25 10:12:15 -0300

    Fazendo o exercicio 2

---
+++ Aula 03-18/Exercicio_2.js
@@ -0,0 +1,39 @@
+db.menu.insertMany([
+    { _id: 1, dish: "Pizza", ingredientes: ["Dough", "Tomato Sauce", "Cheese"], price: 30},
+    { _id: 2, dish: "Sushi", ingredientes: ["Rice", "Fish", "Seaweed"], price: 40},
+    { _id: 3, dish: "Taco", ingredientes: ["Tortilla", "Beef", "Cheese"], price: 15},
+]);
+
+// |--- Exercicio ---|
+// |- A): O restaurante decidiu aumentar o preço de todos os pratos em 10%-|
+// |- Atualizar os preços -|
+
+db.menu.updateMany( 
+    { dish: true},
+    {$mul: { preco: 1.1} 
+});
+
+// |--- Exercicio ---|
+// |- B): O taco agora vem com guacamole-|
+// |- Adicionar ingrediente -|
+db.menu.updateOne( 
+    {dish: "Taco"},
+    {$addToSet: { ingredientes: "Guacamole"}}
+);
+
+// |--- Exercicio ---|
+// |- C): O Sushi teve reajuste no preço-|
+// |- Atualizar o preço do Sushi -|
+db.menu.updateOne(
+    {dish: "Sushi"},
+    {$set: {preco: 35}}
+);
+
+// |--- Exercicio ---|
+// |- D): O restaurante removeu "Beef" dos tacos e colocou Chicken no lugar-|
+// |- Substitui ingrediente do taco -|
+db.menu.updateOne(
+    {dish: "Taco"},
+    {$pull: {ingredientes: "Beef"}},
+    {$push: {ingredientes: "Chicken"}}
+);
